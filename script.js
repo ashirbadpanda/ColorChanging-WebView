504cd8ca25cbe209d575ee712dd4421f6e61e7d1
ashirbadpanda2580 <ashirbadpanda2580@gmail.com>
2024-08-23 16:46:48 +0530

    colorChanging Desktop view

---
+++ script.js
@@ -0,0 +1,25 @@
+let h1 = document.querySelector("h1")
+
+function violet(){
+    h1.style.backgroundColor="violet"
+}
+
+function indigo(){
+    h1.style.backgroundColor="indigo"
+}
+
+function blue(){
+    h1.style.backgroundColor="blue"
+}
+
+function green(){
+    h1.style.backgroundColor="green"
+}
+
+function yellow(){
+    h1.style.backgroundColor="yellow"
+}
+
+function red(){
+    h1.style.backgroundColor="red"
+}
